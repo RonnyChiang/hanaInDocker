dcdfe2fc0d1b99a31215b54f126dafbac7d46ea3
ronnychiang <ronnychiang1164@gmail.com>
2023-01-11 21:06:25 +0800

    feat: all

--- config/passport.js
+++ config/passport.js
@@ -1,104 +1,119 @@
-const passport = require("passport")
-const LocalStrategy = require("passport-local").Strategy
-const FacebookStrategy = require('passport-facebook').Strategy
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require("../models/user")
-const bcrypt = require('bcryptjs')
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-module.exports = app => {
-  app.use(passport.initialize())
-  app.use(passport.session())
+module.exports = (app) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  passport.use(new LocalStrategy({
-    usernameField: "email",
-    passReqToCallback: true,
-  }, (req, email, password, done) => {
-    User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          return done(null, false, req.flash("loginError", "Email or Password incorrect."))
-        }
-        return bcrypt.compare(password, user.password)
-          .then(isMatch => {
-            if (!isMatch) {
-              return done(null, false, req.flash("loginError", "Email or Password incorrect."))
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passReqToCallback: true,
+      },
+      (req, email, password, done) => {
+        User.findOne({ email })
+          .then((user) => {
+            if (!user) {
+              return done(
+                null,
+                false,
+                req.flash("loginError", "Email or Password incorrect.")
+              );
             }
-            return done(null, user)
+            return bcrypt.compare(password, user.password).then((isMatch) => {
+              if (!isMatch) {
+                return done(
+                  null,
+                  false,
+                  req.flash("loginError", "Email or Password incorrect.")
+                );
+              }
+              return done(null, user);
+            });
           })
-      })
-      .catch(err => {
-        res.render(
-          'errorPage',
-          { status: 500, error: err.message }
-        )
-      })
-  }))
+          .catch((err) => done(err, false));
+      }
+    )
+  );
 
   // facebook auth
-  passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK,
-    profileFields: ['email', 'displayName']
-  },
-    (accessToken, refreshToken, profile, done) => {
-      const { name, email } = profile._json
-      User.findOne({ email })
-        .then(user => {
-          if (user) return done(null, user)
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ["email", "displayName"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user);
 
-          const randomPassword = Math.random().toString(36).slice(-8)
+          const randomPassword = Math.random().toString(36).slice(-8);
 
           bcrypt
             .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
-            .then(salt => bcrypt.hash(randomPassword, salt)) // 為使用者密碼「加鹽」，產生雜湊值
-            .then(hash => User.create({
-              name,
-              email,
-              password: hash // 用雜湊值取代原本的使用者密碼
-            }))
-            .then(user => done(null, user))
-            .catch(err => done(err, false))
-        })
-    }
-  ));
+            .then((salt) => bcrypt.hash(randomPassword, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+            .then((hash) =>
+              User.create({
+                name,
+                email,
+                password: hash, // 用雜湊值取代原本的使用者密碼
+              })
+            )
+            .then((user) => done(null, user))
+            .catch((err) => done(err, false));
+        });
+      }
+    )
+  );
 
   // google auth
 
-  passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-    function (accessToken, refreshToken, profile, done) {
-      const { name, email } = profile._json
-      User.findOne({ email })
-        .then(user => {
-          if (user) return done(null, user)
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK,
+      },
+      function (accessToken, refreshToken, profile, done) {
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user);
 
-          const randomPassword = Math.random().toString(36).slice(-8)
+          const randomPassword = Math.random().toString(36).slice(-8);
 
           bcrypt
             .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
-            .then(salt => bcrypt.hash(randomPassword, salt)) // 為使用者密碼「加鹽」，產生雜湊值
-            .then(hash => User.create({
-              name,
-              email,
-              password: hash // 用雜湊值取代原本的使用者密碼
-            }))
-            .then(user => done(null, user))
-            .catch(err => done(err, false))
-        })
-    }
-  ));
+            .then((salt) => bcrypt.hash(randomPassword, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+            .then((hash) =>
+              User.create({
+                name,
+                email,
+                password: hash, // 用雜湊值取代原本的使用者密碼
+              })
+            )
+            .then((user) => done(null, user))
+            .catch((err) => done(err, false));
+        });
+      }
+    )
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean()
-      .then(user => done(null, user))
-      .catch(err => done(err, null))
-  })
-}
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null));
+  });
+};
---
+++ mongo-init.js
@@ -0,0 +1,10 @@
+db.createUser({
+  user: "user1",
+  pwd: "user1pw",
+  roles: [
+    {
+      role: "readWrite",
+      db: "expense-tracker",
+    },
+  ],
+});
